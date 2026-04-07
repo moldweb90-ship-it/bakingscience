157d286a149f9926483043d871997f441f537058
moldweb90-ship-it <mold.web90@gmail.com>
2026-04-07 13:46:39 +0300

    Fix Yandex.Metrika counter: use exact script tag instead of Next.js Script component

--- app/layout.tsx
+++ app/layout.tsx
@@ -57,6 +57,16 @@ export default function RootLayout({
         </main>
         <Footer />
         <CookieConsent />
+        {/* Yandex.Metrika */}
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};m[i].l=1*new Date();for(var j=0;j<document.scripts.length;j++){if(document.scripts[j].src===r){return;}}k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=108426727', 'ym');ym(108426727, 'init', {ssr:true, webvisor:true, clickmap:true, referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});`
+          }}
+        />
+        <noscript>
+          <div><img src="https://mc.yandex.ru/watch/108426727" style={{position:'absolute', left:'-9999px'}} alt="" /></div>
+        </noscript>
         {/* Google Analytics 4 */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-5QMXE3P83M"
